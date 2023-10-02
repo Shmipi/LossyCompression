6e185f3d726830188f4d26b193e26007e4f72531
Ludvig Burud <ludvigburud@gmail.com>
2023-10-02 14:52:03 +0200

    Create PickUp.cs

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private float initialPosition;
+    private float newPosition;
+    public float offset = 9;
+    
+    
+    void Start()
+    {
+        initialPosition = transform.position.y;
+        newPosition = initialPosition + offset;
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.CompareTag("Player");
+
+        Debug.Log("wow a pickUp!");
+
+        Destroy(gameObject);
+    }
+}
